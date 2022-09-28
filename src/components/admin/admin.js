eda8645a143732634e3c6b123e9c8991adafaf72
Tien Minh Vy <tienminhvydev@gmail.com>
2022-09-28 20:31:51 +0700

    hoàn tất giao diện mobile admin

---
+++ src/components/admin/admin.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', e => {
+    let nav_open = document.querySelector("#nav_open");
+    let nav_close = document.querySelector("#nav_close");
+    let nav = document.querySelector("#nav");
+
+    nav_open.addEventListener('click', e => {
+        console.log('hello');
+        nav.classList.add("opened_nav");
+    })
+    nav_close.addEventListener('click', e => {
+        nav.classList.remove("opened_nav");
+    })
+})
