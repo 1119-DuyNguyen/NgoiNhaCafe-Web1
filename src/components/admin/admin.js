97d2151031353f07c30c10fc9f90b8393b92917d
Tien Minh Vy <tienminhvydev@gmail.com>
2022-10-13 20:03:39 +0700

    hoàn tất CSR admin - phần data (sample)

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -4,12 +4,27 @@ document.addEventListener('DOMContentLoaded', e => {
     let nav_open = document.querySelector("#nav_open");
     let nav_close = document.querySelector("#nav_close");
     let nav = document.querySelector("#nav");
+    let under_nav = document.querySelector("#under-nav");
 
+    let nav_lis = document.querySelectorAll("#nav li");
+
+    nav_lis.forEach(elem => {
+        elem.addEventListener('click', e => {
+            nav.classList.toggle("opened_nav");
+            under_nav.classList.toggle("opened_nav");
+        })
+    })
     nav_open.addEventListener('click', e => {
         nav.classList.add("opened_nav");
+        under_nav.classList.add("opened_nav");
     })
     nav_close.addEventListener('click', e => {
         nav.classList.remove("opened_nav");
+        under_nav.classList.remove("opened_nav");
+    })
+    under_nav.addEventListener('click', e => {
+        nav.classList.remove("opened_nav");
+        under_nav.classList.remove("opened_nav");
     })
 
     runCSR();
--- src/components/admin/csr.js
+++ src/components/admin/csr.js
@@ -1,4 +1,156 @@
+import { dataUsers, dataImgs } from "./temp_data.js";
+
+function renderData(page = 1, type = '') {
+    let html, first, last;
+
+    function renderUsers(page) {
+        let s_users = document.querySelector(".admin-container[data-csr='users'] table");
+        html = `<tr>
+            <th></th>
+            <th>STT</th>
+            <th>Tên đăng nhập</th>
+            <th>Email</th>
+            <th>Ngày đăng ký</th>
+            <th>Số điện thoại</th>
+            <th></th>
+        </tr>`;
+        
+        first = page * 10 - 10;
+        last = page * 10 - 1;
+        if (last > dataUsers.length) last = dataUsers.length-1;
+        for (let i = first; i <= last; i++) {
+            html += `<tr>
+                <td>
+                    <input type="checkbox" value="${dataUsers[i].id}" class="user-checkbox"/>
+                </td>
+                <td>${i+1}</td>
+                <td>${dataUsers[i].username}</td>
+                <td>
+                    <a href="mailto:${dataUsers[i].email}">${dataUsers[i].email}</a>
+                </td>
+                <td></td>
+                <td>${dataUsers[i].phone}</td>
+                <td>
+                    <button class="btn btn-info"><i class="icon-pencil"></i></button>
+                    <button class="btn btn-info"><i class="icon-info"></i></button>
+                    <button class="btn btn-danger"><i class="icon-bin"></i></button>
+                </td>
+            </tr>`
+        }
+    
+        s_users.innerHTML = html;
+    }
+    function renderProducts(page) {
+        let s_products = document.querySelector(".admin-container[data-csr='products'] table");
+
+        html = `<tr>
+            <th></th>
+            <th>STT</th>
+            <th>Ảnh</th>
+            <th>Tiêu đề</th>
+            <th>Mô tả</th>
+            <th>Giá</th>
+            <th>Tag</th>
+            <th>Hành động</th>
+        </tr>`;
+    
+        first = page * 10 - 10;
+        last = page * 10 - 1;
+    
+        if (last > dataImgs.length) last = dataImgs.length-1;
+        for (let i = first; i <= last; i++) {
+            html += `<tr>
+                <td>
+                    <input type="checkbox" value="1" class="user-checkbox"/>
+                </td>
+                <td>${i+1}</td>
+                <td><img src="../${dataImgs[i].image}"></td>
+                <td>${dataImgs[i].title}</td>
+                <td>
+                    <div>
+                        ${dataImgs[i].description}
+                    </div>
+                </td>
+                <td>${dataImgs[i].price}${dataImgs[i].currency}</td>
+                <td>${dataImgs[i].tag}</td>
+                <td>
+                    <button class="btn btn-info">
+                        <span class="icon-pencil"></span>
+                    </button>
+                    <button class="btn btn-info">
+                        <span class="icon-info"></span>
+                    </button>
+                    <button class="btn btn-danger">
+                        <span class="icon-bin"></span>
+                    </button>
+                </td>
+            </tr>`
+        }
+    
+        s_products.innerHTML = html;
+    }
+
+    switch (type) {
+        case 'users':
+            renderUsers(page);
+            break;
+
+        case 'products':
+            renderProducts(page);
+            break;
+    
+        default:
+            renderUsers(page);
+            renderProducts(page);
+            break;
+    }
+}
+
+function renderPaginator() {
+    let html, num;
+    let s_users = document.querySelector(".admin-container[data-csr='users'] .paginator_items");
+    
+    num = Math.ceil(dataUsers.length/10);
+    html = '';
+    for (let i = 1; i <= num; i++) {
+        html += `<button data-page="${i}">${i}</button>`;
+    }
+    s_users.innerHTML = html;
+
+    // 
+
+    let s_products = document.querySelector(".admin-container[data-csr='products'] .paginator_items");
+
+    num = Math.ceil(dataImgs.length/10);
+    html = '';
+    for (let i = 1; i <= num; i++) {
+        html += `<button data-page="${i}">${i}</button>`;
+    }
+    s_products.innerHTML = html;
+}
+
 function runCSR() {
+
+    // Render data
+
+    renderData();
+    renderPaginator();
+
+    let p_users = document.querySelectorAll(".admin-container[data-csr='users'] .paginator_items button");
+    let p_products = document.querySelectorAll(".admin-container[data-csr='products'] .paginator_items button");
+
+    p_users.forEach(elem => {
+        elem.addEventListener('click', e => {
+            renderData(elem.dataset.page, 'users');
+        })
+    });
+
+    p_products.forEach(elem => {
+        elem.addEventListener('click', e => {
+            renderData(elem.dataset.page, 'products');
+        })
+    });
+
     // CSR Init
 
     // Lấy data từ URL
