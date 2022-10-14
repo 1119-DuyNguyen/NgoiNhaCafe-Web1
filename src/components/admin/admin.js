14184385d60936f2ac7ea79a3c3ff17df1eb3895
Nguyen Thanh Duy <62139508+1119-DuyNguyen@users.noreply.github.com>
2022-10-14 10:58:24 +0700

    Admin csr (#18)
    CSR data 2 trang user và product cùng với tổng quan giao diện
    
    Co-authored-by: Tien Minh Vy <tienminhvydev@gmail.com>
    Co-authored-by: Minh Huy <huylekore.071103@gmail.com>

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,30 +1,32 @@
+import runCSR from "./csr.js";
+
 document.addEventListener('DOMContentLoaded', e => {
     let nav_open = document.querySelector("#nav_open");
     let nav_close = document.querySelector("#nav_close");
     let nav = document.querySelector("#nav");
+    let under_nav = document.querySelector("#under-nav");
 
+    let nav_lis = document.querySelectorAll("#nav li");
+
+    nav_lis.forEach(elem => {
+        elem.addEventListener('click', e => {
+            nav.classList.toggle("opened_nav");
+            under_nav.classList.toggle("opened_nav");
+        })
+    })
     nav_open.addEventListener('click', e => {
-        console.log('hello');
         nav.classList.add("opened_nav");
+        under_nav.classList.add("opened_nav");
     })
     nav_close.addEventListener('click', e => {
         nav.classList.remove("opened_nav");
+        under_nav.classList.remove("opened_nav");
+    })
+    under_nav.addEventListener('click', e => {
+        nav.classList.remove("opened_nav");
+        under_nav.classList.remove("opened_nav");
     })
 
-    // Quản lý user
-
-    let select_all = document.querySelector("#check-all");
-    let checkboxes = document.querySelectorAll(".user-checkbox");
-
-    select_all.addEventListener('change', e => {
-        if (select_all.checked) {
-            checkboxes.forEach(checkbox => {
-                checkbox.checked = true;
-            })
-        } else {
-            checkboxes.forEach(checkbox => {
-                checkbox.checked = false;
-            })
-        }
-    })
+    runCSR();
+    
 })
