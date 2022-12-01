18c7e5a0fa8b023c939e6cc1f7aaaa21581233e0
duyvosg <vokhuongduy912@gmail.com>
2022-12-01 23:38:19 +0700

    fix lỗi đăng xuất admin

--- admin/js/components/logout.js
+++ admin/js/components/logout.js
@@ -1,15 +1,17 @@
 /**
  * Hàm đăng xuất
  */
+import { Data } from '../../../src/database/data.js';
+var data = new Data();
 function logout() {
-    document.querySelector("#logout").addEventListener('click', e => {
+    document.querySelector('#logout').addEventListener('click', (e) => {
         e.preventDefault();
-        if (confirm("Bạn có muốn đăng xuất?")) {
-            data.setCurrentUser("");
-            data.setAdminNotify("");
-            window.location.href = "../";
+        if (confirm('Bạn có muốn đăng xuất?')) {
+            data.setCurrentUser('');
+            data.setAdminNotify('');
+            window.location.href = '../';
         }
-    })
+    });
 }
 
 export default logout;
